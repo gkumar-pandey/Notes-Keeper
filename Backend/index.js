6b75e8e3f02a392038966886ad89425d91777d89
gkumar-pandey <gautamshekhar078@gmail.com>
2022-09-14 10:30:25 +0530

    update backend and initialize frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const notes = require("./Data/Data");
 const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.listen(5000, () => {
-  console.log("server is running at port 5000");
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+
+  const note = notes.find((note) => id == note._id);
+  res.json(note);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
 });
