a1ed3328741522f5920cf2d3d9deafcb3b988b98
gkumar-pandey <gautamshekhar078@gmail.com>
2022-09-16 15:40:26 +0530

    user authentication backend completed

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,12 @@ const notes = require("./Data/Data");
 const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./Config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./Middleware/errorMiddleware");
 dotenv.config();
 
 connectDB();
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
@@ -15,13 +17,16 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const id = req.params.id;
+// app.get("/api/notes/:id", (req, res) => {
+//   const id = req.params.id;
 
-  const note = notes.find((note) => id == note._id);
-  res.json(note);
-});
-
+//   const note = notes.find((note) => id == note._id);
+//   res.json(note);
+// });
+app.use("/api/users", userRoutes);
+// app.use('/api/login', )
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
