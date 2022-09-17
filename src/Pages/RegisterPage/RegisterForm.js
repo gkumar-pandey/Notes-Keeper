35fa890c5c9b7aae3961cbaee86c882980d17590
gkumar-pandey <gautamshekhar078@gmail.com>
2022-09-17 17:02:45 +0530

    register page ui created

---
+++ src/Pages/RegisterPage/RegisterForm.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const RegisterForm = ({
+  email,
+  name,
+  password,
+  setPassword,
+  setEmail,
+  setName,
+  confirmPassword,
+  setConfirmPassword,
+  picture,
+  setPicture,
+  submitHandler,
+}) => {
+  return (
+    <div>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            type="text"
+            placeholder="Enter your Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email </Form.Label>
+          <Form.Control
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Enter your email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            type="password"
+            placeholder="Confirm Password"
+          />
+        </Form.Group>
+        {/* for picture upload */}
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Profile Picture</Form.Label>
+          <Form.Control
+            type="file"
+            value={picture}
+            onChange={(e) => setPicture(e.target.files[0])}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+      <div className="mt-3">
+        <span>Have an Account ? </span> <Link to={"/login"}>Login here</Link>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterForm;
--- src/Pages/RegisterPage/RegisterPage.js
+++ src/Pages/RegisterPage/RegisterPage.js
@@ -1,7 +1,37 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Mainscreen from "../../Components/MainScreen/Mainscreen";
+import RegisterForm from "./RegisterForm";
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [picture, setPicture] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <Mainscreen title={"Register"}>
+        <RegisterForm
+          email={email}
+          name={name}
+          password={password}
+          picture={picture}
+          setEmail={setEmail}
+          setPassword={setPassword}
+          setName={setName}
+          confirmPassword={confirmPassword}
+          setConfirmPassword={setConfirmPassword}
+          setPicture={setPicture}
+          submitHandler={submitHandler}
+        />
+      </Mainscreen>
+    </div>
+  );
 };
 
 export default RegisterPage;
